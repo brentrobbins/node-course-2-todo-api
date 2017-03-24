0469dc866d3b06b06acca0850b2368d4bc0248d8
brentrobbins <brent@variantstudios.com>
2017-03-24 01:48:07 -0600

    Add GET /todos/:id api route.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //Library Imports
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //Local Imports
 var {mongoose} = require('./db/mongoose');
@@ -31,6 +32,25 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        // Valid id using isn't Valid
+        return res.status(400).send();
+    }
+    User.findById(id).then((todo)=> {
+        if(!todo) {
+            res.status(404).send();
+        }
+        res.send({todo});
+        //console.log('todo found', todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });            
 });
 
 app.listen(3000, () => {
